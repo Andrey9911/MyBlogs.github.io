082ceed225a13048e0c44b3d54c80e5a7dda8b96
Andrey9911 <chivirevandrey775@gmail.com>
2023-02-18 14:59:54 +0300

    header finish

---
+++ src/blocks/header/header.js
@@ -0,0 +1,10 @@
+let drop_block = document.querySelector('.profile-option');
+let img_profile = document.querySelector('.profile-drop');
+
+if(img_profile.querySelector('.profile-img').classList.contains('profile-img_isLogup'))
+{
+    img_profile.addEventListener('mouseenter', () => {
+        drop_block.classList.add('profile-drop_isActive')})
+    img_profile.addEventListener('mouseleave', () => {
+        drop_block.classList.remove('profile-drop_isActive')})
+}
---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+let app = require('webpack');
+
+app.exports({
+    
+})
